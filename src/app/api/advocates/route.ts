415b75ca154d8084f3dcbcc04ec4234705bcdb90
Andy Amaya <contact@andyamaya.com>
2025-11-05 14:54:09 -0800

    Adding basic error handling and explicit types to the advocates api route.

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,22 @@
 import db from "../../../db";
-import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { advocates, type Advocate } from "../../../db/schema";
+import { NextResponse } from "next/server";
+// import { advocateData } from "../../../db/seed/advocates";
 
-export async function GET() {
+type AdvocateResponse = NextResponse<{ data: Advocate[]}> | NextResponse<{ error: string }>;
+
+export async function GET(): Promise<AdvocateResponse> {
   // Uncomment this line to use a database
-  const data = await db.select().from(advocates);
 
+  try {
+    const data = await db.select().from(advocates);
+    return NextResponse.json({ data });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({
+      error: "There was an error returning the advocates" },
+      { status: 500 }
+    );
+  }
   // const data = advocateData;
-
-  return Response.json({ data });
 }
